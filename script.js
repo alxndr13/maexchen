259206f353a95e45b7d9b22cc5babd1721fd8788
Alexander Huck <alexhuck01@gmail.com>
2015-10-22 15:52:17 +0200

    zufälliges würfeln implementiert - der Wahrheit/Lüge Screen geht noch nicht! Derzeit sind Buttons als Platzhalter implementiert

---
+++ script.js
@@ -0,0 +1,9 @@
+var player1,
+    player2,
+    zahlen = Array(11, 21, 22, 31, 32, 33, 41, 42, 43, 44, 51, 52, 53, 54, 55, 61, 62, 63, 64, 65, 66),
+    gewZahl;
+
+function wuerfeln() {
+    gewZahl = zahlen[Math.floor(Math.random() * zahlen.length)];
+    document.getElementById("gewuerfelteZahl").innerHTML = gewZahl;
+}
